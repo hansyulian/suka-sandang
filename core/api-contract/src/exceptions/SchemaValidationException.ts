13957acf492dbde9b3041910651c71bfaad3c978
Hans Yulian <hansyulian@windowslive.com>
2024-10-18 13:09:42 +0700

    fixed exception detection

--- core/api-contract/src/exceptions/SchemaValidationException.ts
+++ core/api-contract/src/exceptions/SchemaValidationException.ts
@@ -13,6 +13,7 @@ export abstract class Exception<Details extends object = {}> extends Error {
   public reference?: string;
   public name: string;
   public details: Details;
+  public static _isException: boolean = true;
 
   public constructor(name: string, details: Details, reference?: string) {
     super(

2ead1901cb6ce056b4ad0d1833ee22c0040867db
Hans Yulian <hansyulian@windowslive.com>
2024-11-01 14:12:36 +0700

    remove unused exports

--- app/common/src/serviceContracts/material/index.ts
+++ app/common/src/serviceContracts/material/index.ts
@@ -1,20 +1,6 @@
-import { CreateMaterialContract } from "./CreateMaterial";
-import { DeleteMaterialContract } from "./DeleteMaterial";
-import { GetMaterialContract } from "./GetMaterial";
-import { ListMaterialsContract } from "./ListMaterials";
-import { UpdateMaterialContract } from "./UpdateMaterial";
-
 export * from "./CreateMaterial";
 export * from "./DeleteMaterial";
 export * from "./GetMaterial";
 export * from "./ListMaterials";
 export * from "./UpdateMaterial";
 export * from "./GetMaterialOptions";
-
-export namespace MaterialContracts {
-  export type CreateMaterial = CreateMaterialContract;
-  export type DeleteMaterial = DeleteMaterialContract;
-  export type GetMaterial = GetMaterialContract;
-  export type ListMaterials = ListMaterialsContract;
-  export type UpdateMaterial = UpdateMaterialContract;
-}
--- app/common/src/serviceContracts/session/index.ts
+++ app/common/src/serviceContracts/session/index.ts
@@ -1,15 +1,4 @@
-import { EmailLoginContract } from "./EmailLogin";
-import { GetUserInfoContract } from "./GetUserInfo";
-import { UpdateUserInfoContract } from "./UpdateUserInfo";
-import { LogoutContract } from "./Logout";
-
 export * from "./GetUserInfo";
 export * from "./EmailLogin";
 export * from "./UpdateUserInfo";
 export * from "./Logout";
-export namespace SessionContracts {
-  export type GetUserInfo = GetUserInfoContract;
-  export type EmailLogin = EmailLoginContract;
-  export type UpdateUserInfo = UpdateUserInfoContract;
-  export type Logout = LogoutContract;
-}
